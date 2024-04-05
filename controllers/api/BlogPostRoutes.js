fd5aad5ebb7bfc32f23f7aaf8cd684035dfd7c10
wellheythereMJ <abetterplace@outlook.com>
2024-04-05 10:12:45 -0700

    set comments and blogpost routes

---
+++ controllers/api/BlogPostRoutes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const { BlogPost } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newBlogPost = await BlogPost.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlogPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await BlogPost.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: 'No blog found with this id!' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await BlogPost.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: 'No blog found with this id!' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const blogPostRoutes = require('./BlogPostRoutes');
+const commentRoutes = require('./CommentRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/blogPost', blogPostRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
