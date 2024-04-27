ab0d3d39b7d91f8d2b146a4569e1b6c08ba19b25
wellheythereMJ <abetterplace@outlook.com>
2024-04-26 22:43:02 -0700

    added comment.js file and setup login in handlebars for comments, need to do more in comment.js, see notes

---
+++ public/js/comment.js
@@ -0,0 +1,9 @@
+document.querySelector("#commentBtn").addEventListener("click", function (event)
+{
+ console.log(event.target)
+})
+
+// get data set 
+// get user imput 
+// fetch call fetch comment routes, the one to add a comment
+// comment route will allow to add post to database
