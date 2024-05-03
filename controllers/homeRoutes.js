2b7eaf830137e33d53cf4aee8cf66ae445f4f397
wellheythereMJ <abetterplace@outlook.com>
2024-05-03 10:53:29 -0700

    completed logic for comments to save and display

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -46,6 +46,7 @@ router.get('/blog/:id', async (req, res) => {
     });
 
     const blog = blogData.get({ plain: true });
+    console.log(blog)
 
     res.render('blog', {
       ...blog,
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,9 +1,31 @@
 document.querySelector("#commentBtn").addEventListener("click", function (event)
 {
  console.log(event.target)
+const blog_post_id = event.target.dataset.id;
+const comment = document.querySelector("#comment").value
+console.log(blog_post_id, comment)
+fetch("/api/comment/", {
+  method: "POST",
+  body: JSON.stringify({ blog_post_id, comment }),
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+ .then((response) => response.json())
+ .then((data) => {
+    console.log(data);
+    document.location.reload();
+  })
+   .catch((err) => {
+      console.log(err);
+    });
 })
 
+
 // get data set 
+
 // get user imput 
+
 // fetch call fetch comment routes, the one to add a comment
+
 // comment route will allow to add post to database
